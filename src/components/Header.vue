b460b02395488cdb0775b24cf3a15fdb69da1f49
blacktunes <1719513828@qq.com>
2019-10-10 13:56:38 +0800

    初始化项目

---
+++ src/components/Header.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="header">
+    <el-row type="flex" align="middle">
+      <el-col :span="2" :offset="2">
+        <i class="el-icon-box header-logo"></i>
+      </el-col>
+      <el-col :span="18">
+        <el-menu :default-active="activeIndex"
+                class="header-menu"
+                mode="horizontal"
+                background-color="#eee"
+                @select="handleSelect">
+        <el-menu-item index="1">主页</el-menu-item>
+        <el-menu-item index="2">笔记</el-menu-item>
+      </el-menu>
+      </el-col>
+    </el-row>
+
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  data () {
+    return {
+      activeIndex: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+.header
+  background #eee
+  .header-logo
+    font-size 45px
+</style>
---
+++ src/plugins/element.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import {
+  Button,
+  Menu,
+  MenuItem,
+  MenuItemGroup,
+  Submenu,
+  Row,
+  Col
+} from 'element-ui'
+
+Vue.use(Button)
+Vue.use(Menu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Submenu)
+Vue.use(Row)
+Vue.use(Col)
