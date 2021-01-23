072e6d054c946743eaf9bfa8d44ff24d62e2cb14
blacktunes <blacktunes@foxmail.com>
2021-01-23 17:10:07 +0800

    优化入口组件显示效果

--- src/views/Entry.vue
+++ src/views/Entry.vue
@@ -1,17 +1,21 @@
 <template>
   <transition name="fade" appear>
-    <div class="wrapper" :class="isRouter ? 'light' : 'dark'" @click="goHome">
-      <div class="message" :class="{ out: isRouter }">
+    <div
+      class="wrapper"
+      :class="isOK || isRouter ? 'light' : 'dark'"
+      @click="goHome"
+    >
+      <div class="message" :class="{ out: isOK }">
         <div class="code">
           <span style="font-weight: 600">{{ code + " " }}</span>
           <transition name="fade">
             <span v-if="showTip" class="tip">{{ tip }}</span>
           </transition>
         </div>
-        <div v-if="!isRouter && msgList.length > 0">
+        <div v-if="!isOK && msgList.length > 0">
           <div v-for="(msg, key) in msgList" :key="msg + key">{{ msg }}</div>
         </div>
-        <div v-if="!isRouter" class="message-animation">
+        <div v-if="!isOK && !isRouter" class="message-animation">
           <div class="code">
             <span style="font-weight: 600">
               {{ code + " " }}
@@ -37,6 +41,10 @@ export default {
       tweenedNumber: 200,
       tip: '',
       codeList: [
+        {
+          code: 200,
+          tip: 'OK'
+        },
         {
           code: 400,
           tip: 'Bad Request'
@@ -123,19 +131,20 @@ export default {
         }
       ],
       msgList: [],
+      isOK: false,
       isRouter: false,
       showTip: true
     }
   },
   methods: {
     goHome() {
-      if (this.isRouter) {
+      if (this.isOK || this.isRouter) {
         this.$router.push('/home')
       } else {
         clearInterval(this.timer)
         this.number = 200
         this.tip = 'OK'
-        this.isRouter = true
+        this.isOK = true
         this.timer = setTimeout(() => {
           this.$router.push('/home')
         }, 1400)
@@ -174,28 +183,37 @@ export default {
   },
   watch: {
     number(newValue) {
+      if (newValue === 200) {
+        this.isRouter = true
+        document.title = '200 OK'
+      } else {
+        this.isRouter = false
+        document.title = `${newValue} ERROR?`
+      }
       gsap.to(this.$data, { duration: 0.5, tweenedNumber: newValue })
     }
   },
   mounted() {
     this.getCode()
-    this.timer = null
     this.setTimer()
+    document.onvisibilitychange = () => {
+      if (document.visibilityState === 'hidden') {
+        clearInterval(this.timer)
+        document.title = '200 OK'
+      } else {
+        this.getCode()
+        this.setTimer()
+      }
+    }
   },
   beforeDestroy() {
     clearInterval(this.timer)
+    document.onvisibilitychange = null
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-$slice-0 = inset(50% 50% 50% 50%)
-$slice-1 = inset(80% 0 0 0)
-$slice-2 = inset(50% 0 30% 0)
-$slice-3 = inset(10% 0 85% 0)
-$slice-4 = inset(40% 0 43% 0)
-$slice-5 = inset(80% 0 5% 0)
-
 @keyframes glitch
   $indent = 100
 
@@ -259,7 +277,7 @@ flex()
       background transparent
       color rgba(255, 255, 255, 0.5)
       text-shadow -3px -3px 0px rgba(248, 240, 5, 0.5), 3px 3px 0px rgba(0, 230, 246, 0.5)
-      clip-path $slice-0
+      clip-path inset(50% 50% 50% 50%)
       animation glitch 25s infinite
       animation-timing-function steps(2, end)
 
